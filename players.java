2acbe8ce9977a5cc970434c95e94323a0a25bdf7
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-05-13 00:08:47 +0530

    Creating player

---
+++ players.java
@@ -0,0 +1,39 @@
+package my_FirstGAME_TicTacToe;
+
+public class players {
+	private String name;
+	private char symbol;
+
+	public players(String name,char symbol) {
+		setName(name);
+		setSymbol(symbol);
+	}
+	
+	
+	public  void setName(String name) {
+		if(!name.isEmpty()) {
+			this.name=name;
+		}
+	}
+
+
+	public String getName() {
+		return this.name;
+	}
+	
+	
+	public void setSymbol(char symbol) {
+		if(symbol!='\0') {
+			this.symbol=symbol;
+
+		}
+	}
+	
+	
+	public char getSymbol() {
+		return this.symbol;
+	}
+
+
+	 
+}
